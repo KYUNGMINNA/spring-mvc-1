dcb72fc63e4e45469a1426949e7a428985a36dd7
NA KYUNG MIN <nkm4095@naver.com>
2023-03-01 14:00:38 +0900

    # 5. 스프링MVC- 구조 이해

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan// 하위 패키지 까지 스캔하여 서블릿이 있으면 등록
 @SpringBootApplication
 public class ServletApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServletApplication.class, args);
     }
---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,16 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component("/springmvc/old-controller")//스프링 빈의 이름으로 URL 매핑
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldContoller handleRequset(request,response)");
+        return new ModelAndView("new-form");
+    }
+}
